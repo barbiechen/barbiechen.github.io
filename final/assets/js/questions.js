babdbb4b62145884b05a04d1e9af1eab351a6340
barbiechen <71330027+barbiechen@users.noreply.github.com>
2021-01-19 01:17:11 +0800

    Add files via upload

---
+++ final/assets/js/questions.js
@@ -0,0 +1,118 @@
+
+const quizQuestions = [
+    {
+        question: "NBA 歷史上一賽季最高勝場數是由哪支球隊達成的?",
+        choices: ["公牛", "湖人", "勇士", "賽爾提克"],
+        correctAnswer: "勇士"
+    },
+    {
+        question: "承上題，勝場數是多少?",
+        choices: ["72", "73", "74", "75"],
+        correctAnswer: "73"
+    },
+
+    {
+        question: "承上題，是在哪個賽季達到的?",
+        choices: ["2015-16", "2016-17", "2018-19", "2019-20"],
+        correctAnswer: "2015-16"
+    },
+
+    {
+        question: "當年2015-16賽季是由哪一隊拿下總冠軍?",
+        choices: ["勇士", "騎士", "火箭", "湖人"],
+        correctAnswer: "騎士"
+    },
+    {
+        question: "NBA歷史上一賽季投進最多三分球是由誰達成的?",
+        choices: ["LeBron James", "James Harden", "Ray Allen", "Stephen Curry"],
+        correctAnswer: "Stephen Curry"
+    },
+    {
+        question: "承上題，投進三分球數量為少顆?",
+        choices: ["289", "305", "402", "405"],
+        correctAnswer: "402"
+    },
+    
+    {
+        question: "2019-20賽季的總冠軍是哪一隊?",
+        choices: ["暴龍", "熱火", "湖人", "快艇"],
+        correctAnswer: "湖人"
+    },
+    {
+        question: "NBA目前(2021)總共有幾支球隊?",
+        choices: ["26", "28", "30", "32"],
+        correctAnswer: "30"
+    },
+    {
+        question: "NBA最久的球隊是哪一支?",
+        choices: ["湖人", "賽爾提克", "熱火", "尼克"],
+        correctAnswer: "賽爾提克"
+    },
+    {
+        question: "誰爸爸沒有打過NBA",
+        choices: ["Kobe Bryant", "Klay Thompson", "Stephen Curry", "Kyrie Irving"],
+        correctAnswer: "Kyrie Irving"
+    },
+    {
+        question: "Michael Jordan 曾經從NBA退休轉而去從事哪項運動?",
+        choices: ["棒球", "美式足球", "足球", "曲棍球"],
+        correctAnswer: "棒球"
+    },
+    {
+        question: "哪一球員在NBA期間沒有轉過隊?",
+        choices: ["James Harden", "Dirk Nowitzki", "Steve Kerr", "Doc Rivers"],
+        correctAnswer: "Dirk Nowitzki"
+    },
+    {
+        question: "NBA史上單場最高分球員?",
+        choices: ["Kobe Bryant", "Wilt Chamberlain", "Devin Booker", "Michael Jordan"],
+        correctAnswer: "Wilt Chamberlain"
+    },
+    {
+        question: "承上題，他那場拿了多少分?",
+        choices: ["70", "80", "100", "105"],
+        correctAnswer: "100"
+    },
+    
+
+    {
+        question: "2019-20賽季的季後賽在那裡舉行?",
+        choices: ["加州", "各球隊球場", "佛羅里達 迪士尼", "環球影城"],
+        correctAnswer: "佛羅里達 迪士尼"
+    }
+
+];
+
+const funImages = [
+    './assets/images/win.gif',
+    './assets/images/win2.gif',
+    './assets/images/win3.gif',
+    './assets/images/win4.gif',
+    './assets/images/win5.gif'
+];
+
+const sadImages = [
+    './assets/images/sad.gif',
+    './assets/images/sad2.gif',
+    './assets/images/sad3.gif',
+    './assets/images/sad4.gif',
+    './assets/images/sad5.gif'
+];
+
+const pictures = [
+    './assets/images/pic1.gif',
+    './assets/images/pic2.jpg',
+    './assets/images/pic3.jpg',
+    './assets/images/pic4.gif',
+    './assets/images/pic5.gif',
+    './assets/images/pic6.jpeg',
+    './assets/images/pic7.gif',
+    './assets/images/pic8.jpeg',
+    './assets/images/pic9.jpg',
+    './assets/images/pic10.gif',
+    './assets/images/pic11.gif',
+    './assets/images/pic12.gif',
+    './assets/images/pic13.jpg',
+    './assets/images/pic14.png',
+    './assets/images/pic15.jpg',
+]
